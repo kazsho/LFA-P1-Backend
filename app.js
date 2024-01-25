e0116658f427d9ca48fbd3428258ef9cf3341ded
henrietta adeniran <henrietta.akindele@gmail.com>
2024-01-25 09:03:39 +0000

    update res.send message

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const cors = require('cors'); 
 const artistsData = require("./artists.json")
 
-console.log(artistsData)
+
 
 
 const app = express();
 app.use(cors());
-app.use(express.json()) 
+app.use(express.json())   
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send("Welcome");
 })
 
 app.get('/painting-recognition', (req, res) => {
