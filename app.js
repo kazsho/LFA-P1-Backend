dd6f26320f199028909e3c0381e16b19412d102f
hortfrancis <ajhf@hotmail.co.uk>
2024-01-25 18:11:15 +0000

    Add endpoint for static mock Picasso data

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 const express = require('express');
-const cors = require('cors'); 
-const artistsData = require("./artists.json")
+const cors = require('cors');
+const artistsData = require("./data/artists.json");
 
-console.log(artistsData)
-
+// Import static mock data for Picasso
+const picassoData = require('./data/artist-bios/pablo-picasso.json');
 
 const app = express();
 app.use(cors());
-app.use(express.json()) 
+app.use(express.json())
+
+
+/* 
+    Endpoint methods
+*/
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
+app.get(
+    '/artist-bio',
+    (req, res) => {
+        // Return static mock data for Picasso
+        res.send(picassoData);
+    }
+)
+
 app.get('/painting-recognition', (req, res) => {
     res.send(artistsData)
 })
-
 
 
 module.exports = app
