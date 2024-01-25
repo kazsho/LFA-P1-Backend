063cac76da9e139c0022c55f2a29e560f7a7555e
hortfrancis <ajhf@hotmail.co.uk>
2024-01-25 20:35:45 +0000

    Refactor /painting-recognition GET endpoint to return data for multiple rounds

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const cors = require('cors');
-const artistsData = require("./data/artists.json");
+// const artistsData = require("./data/artists.json");
 
 // Import static mock data for Picasso
 const picassoData = require('./data/artist-bios/pablo-picasso.json');
@@ -28,8 +28,18 @@ app.get(
 )
 
 app.get('/painting-recognition', (req, res) => {
-    res.send(artistsData)
-})
+    // Get all game data
+    const data = require('./data/painting-recognition/pablo-picasso.json');
 
+    // Check the 'round' key in the query string corresponds to available game data 
+    if (data[(Number(req.query['round']) - 1)] !== undefined) {
+        // Return the game data for this round 
+        res.send(data[(Number(req.query['round']) - 1)]);
+    } else {
+        res.status(404).send({
+            message: "Game data for the requested round number was not found."
+        });
+    }
+});
 
-module.exports = app
+module.exports = app;
